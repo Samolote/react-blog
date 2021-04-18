d2213fedf2610af379d0daba4cf7a136c4c8aa62
Wojciech Łukowski <wojtek304@gmail.com>
2021-04-18 18:30:28 +0200

    Add form component to add new articles

---
+++ src/App/CreateNewBlogPostForm/index.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const CreateNewBlogPostForm: React.FC = () => {
+	const [title, setTitle] = useState<string>('');
+	const [content, setContent] = useState<string>('');
+	const history = useHistory();
+
+	const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value);
+
+	const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
+		setContent(e.target.value);
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const blog = { title, content };
+
+		fetch('http://localhost:3001/blog-posts', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(blog),
+		}).then(() => history.push('/'));
+	};
+
+	return (
+		<div>
+			<h2>Create a new article!</h2>
+			<form onSubmit={handleSubmit}>
+				<div className="field">
+					<label htmlFor="article-title">Article title:</label>
+					<input
+						id="article-title"
+						type="text"
+						value={title}
+						onChange={handleTitleChange}
+						required
+					/>
+				</div>
+				<div className="field">
+					<label htmlFor="article-content">Article content:</label>
+					<textarea
+						id="article-content"
+						value={content}
+						onChange={handleContentChange}
+						required
+					></textarea>
+				</div>
+				<div className="button-wrapper">
+					<input type="submit" value="Add article" />
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default CreateNewBlogPostForm;
