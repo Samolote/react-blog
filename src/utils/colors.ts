ad94ba1f8b9ff15da842ed32c382d2cdefe9510c
Wojciech Łukowski <wojtek304@gmail.com>
2021-04-18 12:35:16 +0200

    Add file containing color palette (SSoT*)
    *Single Source of Truth

---
+++ src/utils/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+	darkGray: '#262626',
+	eerieBlack: '#262626',
+	laurelGreen: '#ACBFA4',
+	lightGray: '#E5E5E5',
+	amber: '#FF7F11',
+	red: '#FF1B1C',
+};
+
+export default colors;
