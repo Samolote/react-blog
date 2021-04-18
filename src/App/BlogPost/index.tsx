5776f66daefaa0a05eede6ed368f5878a225a018
Wojciech Łukowski <wojtek304@gmail.com>
2021-04-18 18:31:02 +0200

    Add article details component

---
+++ src/App/BlogPost/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
+interface BlogPostData {
+	id: number;
+	title: string;
+	content: string;
+	likes?: number;
+	dislikes?: number;
+}
+
+const BlogPost: React.FC = () => {
+	const { id } = useParams<{ id: string }>();
+	const [blogPostData, setBlogPostData] = useState<BlogPostData>();
+	const history = useHistory();
+
+	useEffect(() => {
+		fetch(`http://localhost:3001/blog-posts/${id}`)
+			.then((res) => res.json())
+			.then((data) => setBlogPostData(data));
+	}, [id]);
+
+	const handleEditClick = (e: React.MouseEvent) => {
+		alert(e.currentTarget.textContent);
+	};
+
+	const handleDeleteClick = (e: React.MouseEvent) => {
+		fetch(`http://localhost:3001/blog-posts/${id}`, { method: 'DELETE' }).then(() =>
+			history.push('/')
+		);
+	};
+
+	return (
+		<article>
+			{blogPostData && (
+				<>
+					<h2>{blogPostData.title}</h2>
+					<p>{blogPostData.content}</p>
+					<div className="button-wrapper">
+						<button onClick={handleEditClick}>Edit</button>
+						<button onClick={handleDeleteClick}>Delete</button>
+					</div>
+				</>
+			)}
+		</article>
+	);
+};
+
+export default BlogPost;
