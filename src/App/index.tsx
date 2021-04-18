036636a5642a7e8b2292d09cbed092d89643f8b9
Wojciech Łukowski <wojtek304@gmail.com>
2021-04-18 18:31:51 +0200

    Connect form and details component with main view

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Header from './Header/index';
-import BlogGrid from './BlogGrid/index';
 import TopContainer from '../shared/TopContainer';
 import MainWrapper from '../shared/MainWrapper';
+import Header from './Header/index';
+import BlogGrid from './BlogGrid/index';
+import BlogPost from './BlogPost';
+import CreateNewBlogPostForm from './CreateNewBlogPostForm/index';
 
 const App: React.FC = () => {
 	return (
@@ -17,7 +19,10 @@ const App: React.FC = () => {
 							<BlogGrid />
 						</Route>
 						<Route path="/create-blog-post">
-							<p>yo add some form here</p>
+							<CreateNewBlogPostForm />
+						</Route>
+						<Route path="/blog-post/:id">
+							<BlogPost />
 						</Route>
 						<Route path="*">
 							<p>no such address duuuude - 404</p>
