e58b2a546634ee00daea3f870d6e5cf2a659ed67
Wojciech Łukowski <wojtek304@gmail.com>
2021-04-18 12:32:45 +0200

    Create TopContainer component

---
+++ src/components/TopContainer.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const TopContainer = styled.div`
+	margin: 0 auto;
+	padding-left: 2rem;
+	padding-right: 2rem;
+	max-width: 1200px;
+`;
+
+export default TopContainer;
