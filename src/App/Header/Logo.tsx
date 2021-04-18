4589e3d45d974026ac654bd4f229d123a45acf35
Wojciech Łukowski <wojtek304@gmail.com>
2021-04-18 15:57:40 +0200

    Add bottom gradient border on logo component

--- src/App/Header/Logo.tsx
+++ src/App/Header/Logo.tsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 import colors from '../../utils/colors';
 
 const Logo = styled(Link)`
+	padding: 0 2rem;
+	border-bottom: 5px solid;
+	border-image-slice: 1;
+	border-image-source: linear-gradient(90deg, ${colors.amber} 0%, ${colors.red} 100%);
 	text-decoration: none;
 	color: ${colors.lightGray};
 `;
